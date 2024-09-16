d8dd6badaad96e6b8977c40fec675739b5fad2c6
ShuraKent <ghedc12@gmail.com>
2024-09-16 20:14:48 +0300

    first commit

---
+++ src/test/java/org/example/CalculatorAppTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorAppTest
+    extends TestCase
+{
+    CalculatorApp calc = new CalculatorApp();
+
+    private void testAssertEqualsSum(Double a, Double b, Double expected) {
+        assertEquals(expected, calc.sum(a, b));
+    }
+    private void testAssertEqualsMulti(Double a, Double b, Double expected) {
+        assertEquals(expected, calc.mul(a, b));
+    }
+    private void testAssertEqualsDiv(Double a, Double b, Double expected) {
+        assertEquals(expected, calc.div(a, b));
+    }
+
+    public void testAdd(){
+        testAssertEqualsSum(2.0, 2.0, 4.0);
+    }
+    public void testMulti(){
+        testAssertEqualsMulti(2.0, 2.0, 4.0);
+    }
+    public void testDiv(){
+        testAssertEqualsDiv(4.0, 2.0, 2.0);
+        testAssertEqualsDiv(4.0, 0.0, Double.MAX_VALUE);
+    }
+}
